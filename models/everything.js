04d66e8fdb0932db216d77a6c71e94516bf6f049
Jed Watson <jed.watson@me.com>
2013-09-04 02:02:01 +1000

    Adding various field configurations to the 'everything' list

--- models/everything.js
+++ models/everything.js
@@ -3,7 +3,8 @@ var keystone = require('keystone'),
 
 var Everything = new keystone.List('Everything', {
 	label: 'Everything',
-	singular: 'Thing'
+	singular: 'Thing',
+	autokey: { from: 'name', path: 'autokey' }
 });
 
 Everything.add(
@@ -14,19 +15,19 @@ Everything.add(
 	shortString: { type: String, width: 'short' },
 	mediumString: { type: String, width: 'medium' },
 	longString: { type: String, width: 'long' },
-	textarea: { type: Types.Textarea },
+	textarea: { type: Types.Textarea, initial: true },
 	key: { type: Types.Key },
 	email: { type: Types.Email },
 	url: { type: Types.Url },
 	number: { type: Types.Number },
 	money: { type: Types.Money },
-	checkbox: { type: Boolean },
+	checkbox: { type: Boolean, initial: true },
 	date: { type: Types.Date },
 	dateTime: { type: Date },
 	html: { type: Types.Html }
 }, 'Complex Fields', {
-	wysiwygHtml: { type: Types.Html, wysiwyg: true },
-	select: { type: Types.Select, options: 'first, second, third' },
+	select: { type: Types.Select, options: 'first, second, third', initial: true },
+	indentedCheckbox: { type: Boolean, initial: true, indent: true, note: 'This checkbox is indented' },
 	customSelect: { type: Types.Select, options: [
 		{ label: 'Option 1', value: 'one' },
 		{ label: 'Option 2', value: 'two' },
@@ -37,21 +38,22 @@ Everything.add(
 		{ label: 'Number 2', value: 2 },
 		{ label: 'Number 3', value: 3 }
 	] },
-	splitName: { type: Types.Name },
-	password: { type: Types.Password },
+	splitName: { type: Types.Name, initial: true },
+	password: { type: Types.Password, initial: true },
 	cloudinaryImage: { type: Types.CloudinaryImage },
-	location: { type: Types.Location }
+	location: { type: Types.Location },
+	wysiwygHtml: { type: Types.Html, wysiwyg: true },
+	shortWysiwygField: { type: Types.Html, wysiwyg: true, height: 100 }
 }, 'Relationships', {
-	user: { type: Types.Relationship, ref: 'User' },
+	user: { type: Types.Relationship, ref: 'User', initial: true },
 	users: { type: Types.Relationship, ref: 'User', many: true }
 }, 'Uneditable Fields', {
 	uneditableString: { type: String, noedit: true, default: "Not editable" },
-	uneditableCheckbox: { type: Boolean, noedit: true, default: true }
-});
-
-// Provide access to Keystone
-Everything.schema.virtual('canAccessKeystone').get(function() {
-	return this.isAdmin;
+	uneditableCheckbox: { type: Boolean, noedit: true, default: true, note: 'Uneditable boolean notes are displayed next to the checkbox' },
+	uneditableDate: { type: Types.Date, noedit: true, default: Date.now },
+	uneditableSelect: { type: Types.Select, noedit: true, options: 'Sydney, New York, London, Paris, Hong Kong', default: 'Sydney' },
+	uneditableLocation: { type: Types.Location, noedit: true, defaults: { street1: '283-285 Kent St', suburb: 'Sydney', state: 'NSW', postcode: '2000', country: 'Australia' } },
+	uneditableImage: { type: Types.CloudinaryImage, noedit: true }
 });
 
 Everything.addPattern('standard meta');
