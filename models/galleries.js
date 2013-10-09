6c3075832f05a4bcb75e1fb109a2f8176731f78a
Jed Watson <jed.watson@me.com>
2013-10-09 16:00:37 +1100

    Adding image galleries model

---
+++ models/galleries.js
@@ -0,0 +1,15 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+var Gallery = new keystone.List('Gallery', {
+	autokey: { from: 'name', path: 'key' }
+});
+
+Gallery.add({
+	name: { type: String, required: true },
+	publishedDate: { type: Date, default: Date.now },
+	images: { type: Types.CloudinaryImages }
+});
+
+Gallery.addPattern('standard meta');
+Gallery.register();
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 require('./users');
 require('./posts');
 require('./postCategories');
+require('./galleries');
 require('./enquiries');
 require('./everything');
