35eb689a0773607714099296464a5f1f1e0a1983
Jed Watson <jed.watson@me.com>
2014-01-17 15:06:20 +1100

    Adding front-end editing UI

--- web.js
+++ web.js
@@ -41,6 +41,7 @@ keystone.set('locals', {
 	_: require('underscore'),
 	env: keystone.get('env'),
 	utils: keystone.utils,
+	editable: keystone.content.editable,
 	ga_property: keystone.get('ga property'),
 	ga_domain: keystone.get('ga domain'),
 	chartbeat_property: keystone.get('chartbeat property'),
