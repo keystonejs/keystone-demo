40fa67f972988d35acf1c9b9cbd2f2927238b05f
Harry Moreno <morenoh149@gmail.com>
2015-02-11 13:46:23 -0800

    refactor updates/0.0.1-admins.js to follow modern keystone conventions

--- updates/0.0.1-admins.js
+++ updates/0.0.1-admins.js
@@ -1,28 +1,15 @@
 var keystone = require('keystone'),
-	async = require('async'),
-	User = keystone.list('User');
+		User = keystone.list('User');
 
-var admins = [
-	{ email: 'demo@keystonejs.com', password: 'demo', name: { first: 'Demo', last: 'User' } }
-];
-
-function createAdmin(admin, done) {
-	User.model.findOne({ email: admin.email }).exec(function(err, user) {
-		var newAdmin = new User.model(admin);
-		newAdmin.isAdmin = true;
-		newAdmin.isProtected = true;
-		newAdmin.save(function(err) {
-			if (err) {
-				console.error("Error adding admin " + admin.email + " to the database:");
-				console.error(err);
-			} else {
-				console.log("Added admin " + admin.email + " to the database.");
-			}
-			done();
-		});
-	});
-}
-
-exports = module.exports = function(done) {
-	async.forEach(admins, createAdmin, done);
+module.exports = function(done) {
+	new User.model({
+		name: {
+			first: 'Demo',
+			last: 'User'
+		},
+		email: 'demo@keystonejs.com',
+		password: 'demo',
+		isAdmin: true
+	})
+	.save(done);
 };
