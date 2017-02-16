880eb37003655e4e095437dd518c3799d4bf27fe
Jed Watson <jed.watson@me.com>
2017-02-16 12:47:50 +1100

    Fixes the demo password override issue on startup

--- models/User.js
+++ models/User.js
@@ -40,7 +40,17 @@ function protect (path) {
 	});
 }
 
-['name.first', 'name.last', 'email', 'password', 'isProtected'].forEach(protect);
+['name.first', 'name.last', 'email', 'isProtected'].forEach(protect);
+
+User.schema.path('password').set(function (newValue) {
+	// the setter for the password field is more complicated because it has to
+	// emulate the setter on the password type, and ensure hashing before save
+	// also, we can't currently escape the hash->set loop, so the hash is harcoded
+	// for the demo user for now.
+	if (this.isProtected) return '$2a$10$fMeQ6uNsJhJZnY/6soWfc.Mq8T3MwANJK52LQCK2jzw/NjE.JBHV2';
+	this.__password_needs_hashing = true;
+	return newValue;
+});
 
 /**
  * END DEMO USER PROTECTION
